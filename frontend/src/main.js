1ba026a57174a6fec4f90c9ae204255fdf9be680
Suebwj <1175634156@qq.com>
2024-09-29 14:46:56 +0800

    add vuetify components

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Vuetify from 'vuetify';
 
 
 createApp(App)
     .use(store)
     .use(router)
+    .use(Vuetify)
     .mount('#app');
